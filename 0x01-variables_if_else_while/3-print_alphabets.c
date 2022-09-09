038d7e578d917238133e87138b35e945cd0e4826
Mehdi Masmar <masmar507@gmail.com>
2022-09-09 07:58:31 -0700

    alpABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - main file
+ * alphABET
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+	
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
+		int lowerchar = tolower(ch);
+		
+		putchar(lowerchar);
+	}
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
