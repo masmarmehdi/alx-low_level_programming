be3bf54ff72eedf0b7d4acf012cce3ad055c4a2c
Mehdi Masmar <masmar507@gmail.com>
2022-09-09 14:26:46 -0700

    The success combination in business is: Do what you do better... and: do more of what you do...

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,14 +10,14 @@ int main(void)
 
 	for (i = '0'; i <= '8'; i++)
 	{
-		for (j= i+1; j <='9'; j++)
+		for (j = i + 1; j <= '9'; j++)
 		{
-			for (k=j+1; k <='9'; k++)
+			for (k = j + 1; k <= '9'; k++)
 			{
 				putchar(i);
 				putchar(j);
 				putchar(k);
-				if ( i != '7' || j != '8' || k != '9')
+				if (i != '7' || j != '8' || k != '9')
 				{
 					putchar(',');
 					putchar(' ');
