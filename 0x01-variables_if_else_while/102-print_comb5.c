fc345db662f868641fed7b0a272b4b5efbe640e8
Mehdi Masmar <masmar507@gmail.com>
2022-09-12 03:31:43 -0700

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -18,13 +18,13 @@ int main(void)
 				putchar((i % 10) + 48);
 				putchar(' ');
 				putchar((j / 10) + 48);
-				putchar((j % 10) + 48);		
+				putchar((j % 10) + 48);
 				if (i != 98 || j != 99)
 				{
 					putchar(',');
 					putchar(' ');
 				}
-			}	
+			}
 		}
 	}
 	putchar('\n');
