4ca3b588e3e5edcae89b9feb4fa0aab17aef80b7
Mehdi Masmar <masmar507@gmail.com>
2022-09-13 05:35:44 -0700

    fixing betty coding style

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
  * 1-alphabet - void function
- *
+ * I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game
  * Return: 0 (Success).
  */
 void print_alphabet(void)
 {
 	int i;
-	
+
 	for (i = 'a'; i <= 'z'; i++)
 	{
 		_putchar(i);
