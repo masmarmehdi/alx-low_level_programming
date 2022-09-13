d6da7b7d2dbeaefc1755cb044048d33f427b0cdb
Mehdi Masmar <masmar507@gmail.com>
2022-09-13 16:22:31 -0700

    main file

---
+++ 0x02-functions_nested_loops/10-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - main file
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int n;
+	
+	n = add(89, 9);
+	printf("%d\n", n);
+	return (0);
+}
