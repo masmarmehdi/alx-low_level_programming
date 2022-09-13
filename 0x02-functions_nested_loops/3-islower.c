50ddfae79e3871d8b887e2721205027de4a7b170
Mehdi Masmar <masmar507@gmail.com>
2022-09-13 06:35:21 -0700

    fixing some issue concerning beety coding style

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
  * islower - int function
+ * @c: an int argument
  *
  * Return: 1 if the argument is lowercase or 0 if not
  */
@@ -8,7 +9,7 @@ int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
 	{
-		return 1;
+		return (1);
 	}
 	return (0);
 }
