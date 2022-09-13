2a13bc748719ff4f2bae324e38e69bb03b311873
Mehdi Masmar <masmar507@gmail.com>
2022-09-13 16:22:43 -0700

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - file
+ * @a: first int input
+ * @b: second int input
+ * Description: a +b
+ * Return: result of a + b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
