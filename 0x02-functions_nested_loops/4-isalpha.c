b8677063d0342d3ab22649e257e09de5eb79b6b6
Mehdi Masmar <masmar507@gmail.com>
2022-09-13 07:24:28 -0700

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isalpha - alpha
+ * @c: int argument
+ * Description: checking alphabetic
+ * Return: Always 0
+ */
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <=90) || (c >= 97 && c <= 122))
+	{
+		return (1);
+	}
+	return (0);
+}
