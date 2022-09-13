705ba5de4d92d905d8de04171d8a57591200775f
Mehdi Masmar <masmar507@gmail.com>
2022-09-13 06:32:27 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * islower - int function
+ *
+ * Return: 1 if the argument is lowercase or 0 if not
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return 1;
+	}
+	return (0);
+}
