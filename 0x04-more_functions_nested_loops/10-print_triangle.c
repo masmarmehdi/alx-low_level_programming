10586fdbb6d9dd6f9f6842ad87886ccaeff592d8
Mehdi Masmar <masmar507@gmail.com>
2022-09-16 06:51:31 -0700

    fixing new line issue

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,25 +9,27 @@ void print_triangle(int size)
 {
 	int i, j;
 	int n = size - 1;
-	for (i = 0; i < size; i++)
+	if (size <= 0)
 	{
-		if (size <= 0)
-		{
-			_putchar('\n');
-			break;
-		}
-		for (j = 0; j < size; j++)
-		{
-			if (j < n)
-			{
-				_putchar(' ');
-			}
-			else
-			{
-				_putchar('#');
-			}
-		}
-		n--;
 		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				if (j < n)
+				{
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar('#');
+				}
+			}
+			n--;
+			_putchar('\n');
+		}
 	}
 }
