011c7e809fafe1ee706dd66f4a0ad5a542a4841f
Mehdi Masmar <masmar507@gmail.com>
2022-09-16 03:43:27 -0700

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - int function
+ * @a: first int input
+ * @b: second int input
+ * Description: Collaboration is multiplication
+ * Return: result of multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
