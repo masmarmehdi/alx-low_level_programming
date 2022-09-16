0f392b702f95e9e57cdb57e29198da1ced7641eb
Mehdi Masmar <masmar507@gmail.com>
2022-09-16 08:49:51 -0700

    fixing betty coding style

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * main - prints if integer is positive or negative
- *
- * Return: always 0 (Success).
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
  */
+
 int main(void)
 {
 	int i;
