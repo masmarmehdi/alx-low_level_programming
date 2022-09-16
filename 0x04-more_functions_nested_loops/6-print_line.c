c55df109a7587fab62ac23f17f462aed143fcb79
Mehdi Masmar <masmar507@gmail.com>
2022-09-16 04:20:48 -0700

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_line - void function
+ * @n: int input
+ * Description: The shortest distance between two points is a straight line
+ * Return: nothing
+ */
+void print_line(int n)
+{
+	int i;
+	
+	for (i = 0; i < n; i++)
+	{
+		if( n <= 0)
+		{
+			_putchar('\n');
+			break;
+		}
+		_putchar('_');
+	}
+	_putchar('\n');
+}
