d342661a4001cc277638f8b11273cb38555a8984
Mehdi Masmar <masmar507@gmail.com>
2022-09-16 04:39:11 -0700

    print diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_diagonal - void function
+ * @n: int input
+ * Description: I feel like I am diagonally parked in a parallel universe
+ * Return: nothing
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	for (i = 0; i < n; i++)
+	{
+		for (j = i; j >= 0; j--)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
