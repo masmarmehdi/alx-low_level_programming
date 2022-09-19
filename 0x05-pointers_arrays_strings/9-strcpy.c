f580332180fbdf62ada8a60f78ab3a6f0a1bf863
Mehdi Masmar <masmar507@gmail.com>
2022-09-19 11:07:05 -0700

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strcpy - char * function
+ * @dest: char pointer
+ * @src: char pointer
+ * Description: copy str
+ * Return: pointer to dest.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+	
+	while(src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
