aea5c8090e0b69cb91d61610651d91d42dbedd46
Mehdi Masmar <masmar507@gmail.com>
2022-09-22 10:38:44 -0700

    strcmp: efficient solution than previous

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,19 +8,16 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i, j;
+	int i;
 
-	for (i = 0; s1[i] != '\0'; i++)
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 	{
-		for (j = 0; s2[j] != '\0'; j++)
+		if (s1[i] != s2[i])
 		{
-			if (s1[i] != s2[j])
-			{
-				return (s1[i] - s2[i]);
-			}
-			s1++;
-			s2++;
+			return (s1[i] - s2[i]);
 		}
+		s1++;
+		s2++;
 	}
-	return (s1[i] - s2[j]);
+	return (s1[i] - s2[i]);
 }
