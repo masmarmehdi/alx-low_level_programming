003e964bb72c9d2b5211210ce4d49b1728f1e421
Mehdi Masmar <masmar507@gmail.com>
2022-09-22 16:30:22 -0700

    fixing betty coding style

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,13 +9,9 @@ char *cap_string(char *s)
 {
 	int i, j;
 	char special_characters[] = {' ', ';', '.', '!', '?', '\n', '(', ')', '{', '}', ',', '\t', '"'};
+
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		/*
-		if (i == 0 && (s[i] >= 97 && s[i] <= 122))
-		{
-			s[i] -= 32;
-		}*/
 		for (j = 0; j < 13; j++)
 		{
 			if (s[i] == special_characters[j])
