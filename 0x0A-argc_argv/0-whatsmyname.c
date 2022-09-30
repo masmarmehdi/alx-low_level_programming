7e96bdb5beebd07933b4487da46339d94a8cb62b
Mehdi Masmar <masmar507@gmail.com>
2022-09-30 00:33:35 -0700

    It ain't what they call you, it's what you answer to

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	if (argc == 1)
+	{
+		printf("%s\n", argv[argc - 1]);
+	}
+	return (0);
+}
