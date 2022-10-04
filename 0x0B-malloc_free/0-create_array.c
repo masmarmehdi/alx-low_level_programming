661f6729bcb965a3af8034eebd402a59cd0f9577
Mehdi Masmar <masmar507@gmail.com>
2022-10-04 06:19:37 -0700

    adding another condition of null

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,7 +15,7 @@ char *create_array(unsigned int size, char c)
 	unsigned int  i;
 
 	result = malloc(sizeof(char) * size);
-	if (size == 0)
+	if (size == 0 || result == NULL)
 	{
 		return (NULL);
 	}
