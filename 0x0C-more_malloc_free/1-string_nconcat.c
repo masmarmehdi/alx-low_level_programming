9a63ba04437072655158f1c169a79127418c045e
Mehdi Masmar <masmar507@gmail.com>
2022-10-06 10:56:07 -0700

    using realloc to change size of memory + some conditions

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,24 +12,58 @@
 char * string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *s;
-	unsigned int len = 0, i, j;
-
+	unsigned int len = 0, len_s2 = 0, i, j;
+	
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
 	while (s1[len] != '\0')
 	{
 		len++;
 	}
-	s = malloc(sizeof(char) * (n + len) + 1);
-
-	for (i = 0, j = 0; i < n + len; i++)
+	while (s2[len_s2] != '\0')
 	{
-		if (i < len)
+		len_s2++;
+	}
+	s = malloc(sizeof(char) * (len + len_s2) + 1);
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	if (n >= len_s2)
+	{
+		for (i = 0, j = 0; i < len_s2 + len; i++)
 		{
-			s[i] = s1[i];
+			if (i < len)
+			{
+				s[i] = s1[i];
+			}
+			else
+			{
+				s[i] = s2[j];
+				j++;
+			}
 		}
-		else
+	}
+	else
+	{
+		s = realloc(s, sizeof(char) * (n + len) + 1);
+		for (i = 0, j = 0; i < n + len; i++)
 		{
-			s[i] = s2[j];
-			j++;
+			if (i < len)
+			{
+				s[i] = s1[i];
+			}
+			else
+			{
+				s[i] = s2[j];
+				j++;
+			}
 		}
 	}
 	s[i] = '\0';
