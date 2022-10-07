5d3f13c902d78101e6dfb28cd7033e2c3ce929e8
Mehdi Masmar <masmar507@gmail.com>
2022-10-07 04:06:35 -0700

    calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - void pointer function
+ * @nmemb: unsigned int
+ * @size: unsigned int
+ *
+ * Return: NUll if nmemb or size is 0. otherwiser pointer to alloc mem
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *alloc;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	alloc = malloc(nmemb * size);
+	if (alloc == NULL)
+	{
+		return (NULL);
+	}
+	return (alloc);
+}
