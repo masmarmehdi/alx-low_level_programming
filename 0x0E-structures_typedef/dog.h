ffd9f28ea11ad6c8e1c764f59c60e20210159673
Mehdi Masmar <masmar507@gmail.com>
2022-10-10 05:53:35 -0700

    betty even in header file ?

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef _DOG_H
 #define _DOG_H
-struct dog {
+
+/**
+ * struct dog - objects of a dog
+ * @name: str
+ * @age: float
+ * @owner: str
+ * Description: objects of a dawg
+ */
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
