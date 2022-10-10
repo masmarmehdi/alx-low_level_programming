19eb3654aaf191b18e21ba1ed92e085528c15cff
Mehdi Masmar <masmar507@gmail.com>
2022-10-10 06:03:17 -0700

    adding conditional statement

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdlib.h>
 
 /**
  * init_dog - void function
@@ -11,6 +12,10 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+	{
+		return;
+	}
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
