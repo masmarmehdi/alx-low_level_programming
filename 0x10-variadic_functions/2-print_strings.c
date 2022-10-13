2b802c899d56e209e139955205add35ddf6c6378
Mehdi Masmar <masmar507@gmail.com>
2022-10-13 05:14:07 -0700

    removing extra space

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -23,7 +23,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("%s", value);
 			if (i != n - 1)
 			{
-				printf("%s ", separator);
+				printf("%s", separator);
 			}
 		}
 		printf("\n");
