6ba423aa353df3b839d150b9b538f480d8f56db4
Mehdi Masmar <masmar507@gmail.com>
2022-10-25 16:53:15 -0700

    fixing betty coding style

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -15,7 +15,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	previous_node = *head;
 	if (index != 0)
 	{
-		for (i = 0; i < index -1 && previous_node != NULL; i++)
+		for (i = 0; i < index - 1 && previous_node != NULL; i++)
 		{
 			previous_node = previous_node->next;
 		}
