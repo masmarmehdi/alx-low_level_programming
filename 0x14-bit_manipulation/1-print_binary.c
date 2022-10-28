3221943265100c7f399f2386949d762c4181e2e6
Mehdi Masmar <masmar507@gmail.com>
2022-10-28 16:55:31 -0700

    print binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_binary - void function
+ * @n: unsigned long int
+ *
+ * Return: nothing.
+ */
+void print_binary(unsigned long int n)
+{
+	if (n >> 0)
+	{
+		if (n >> 1)
+		{
+			print_binary(n >> 1);
+		}
+		_putchar((n & 1) + '0');
+	}
+	else
+	{
+		_putchar('0');
+	}
+}
