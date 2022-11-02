d60e8e74e843b76030741f82446df1329a215946
Mehdi Masmar <masmar507@gmail.com>
2022-11-01 17:30:49 -0700

    adding libraries needed for file IO  + adding read_textfile to header file

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,11 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif
